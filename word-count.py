fb02ab2eed37a4f913a2fac31593e06dd3e6e6f0
Santiago Benavidez <santiago.benavidez@cloudhesive.com>
2019-06-23 15:57:07 -0300

    reorder imports

--- word-count.py
+++ word-count.py
@@ -1,9 +1,8 @@
 from __future__ import print_function
+from operator import add
+from pyspark.sql import SparkSession
 
 import sys
-from operator import add
-
-from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
   #  if len(sys.argv) != 2:
