d9139df94b7c3109bca4a343db3b1a844300d7b9
Santiago Benavidez <santiago.benavidez@cloudhesive.com>
2019-06-23 15:16:09 -0300

    Modification of certain params to load from the file system

--- word-count.py
+++ word-count.py
@@ -7,16 +7,18 @@ from pyspark.sql import SparkSession
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: wordcount <file>", file=sys.stderr)
-        sys.exit(-1)
+  #  if len(sys.argv) != 2:
+  #      print("Usage: wordcount <file>", file=sys.stderr)
+  #      sys.exit(-1)
+
+    stringToCount = "/home/sbenavidez/work/pruebaContador.txt"
 
     spark = SparkSession\
         .builder\
-        .appName("PythonWordCount")\
+        .appName("Word Counter test environment ")\
         .getOrCreate()
 
-    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+    lines = spark.read.text(stringToCount).rdd.map(lambda r: r[0])
     counts = lines.flatMap(lambda x: x.split(' ')) \
                   .map(lambda x: (x, 1)) \
                   .reduceByKey(add)
